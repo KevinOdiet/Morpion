3cb4b8af4e156d3e37d950ba2e8f5d21ff6c1468
Kevin Odiet <kevin.odiet@ceff.ch>
2022-10-04 13:54:25 +0200

    amélioration visuel

--- morpion.js
+++ morpion.js
@@ -1,6 +1,7 @@
 $(function (){
     var i = 0
     var array = [0,1,2,3,4,5,6,7,8];
+    $('body').append('<div id="winner"></div>');
 
     $("td").each(function (index, element){
     $(this).click(function () {
@@ -28,13 +29,19 @@ $(function (){
                 array[2] === "X" && array[5] === "X" && array[8] === "X" ||
                 array[0] === "X" && array[4] === "X" && array[8] === "X" ||
                 array[6] === "X" && array[4] === "X" && array[2] === "X" ){
-                setTimeout(() => {
-                    alert("Le joueur croix a gagné")
-                    array = [0,1,2,3,4,5,6,7,8];
+
+                $("#winner").append("Le joueur X a gagné")
+                array = [0,1,2,3,4,5,6,7,8];
+                setTimeout(function () {
                     $("td").removeClass("symbole_0")
                     $("td").removeClass("symbole_1")
                     i = 0
-                }, 100)
+                }, 300)
+
+                setTimeout(function() {
+                    document.getElementById('winner').innerHTML = "";
+                },2000);
+
             }else if (i > 8){
                 var egalite = 1
             }
@@ -47,24 +54,34 @@ $(function (){
                 array[2] === "O" && array[5] === "O" && array[8] === "O" ||
                 array[0] === "O" && array[4] === "O" && array[8] === "O" ||
                 array[6] === "O" && array[4] === "O" && array[2] === "O" ){
-                setTimeout(() => {
-                    alert("Le joueur cercle a gagné")
-                    array = [0,1,2,3,4,5,6,7,8];
+
+                $("#winner").append("Le joueur O a gagné")
+                array = [0,1,2,3,4,5,6,7,8];
+                setTimeout(function () {
                     $("td").removeClass("symbole_0")
                     $("td").removeClass("symbole_1")
                     i = 0
-                }, 100)
+                }, 300)
+
+                setTimeout(function() {
+                    document.getElementById('winner').innerHTML = "";
+                },2000);
+
             }else if (i > 8){
                  egalite++
             }
             if (egalite == 2){
-                setTimeout(() => {
-                    alert("Égalité")
-                    array = [0,1,2,3,4,5,6,7,8];
+                $("#winner").append("Égalité")
+                array = [0,1,2,3,4,5,6,7,8];
+                setTimeout(function () {
                     $("td").removeClass("symbole_0")
                     $("td").removeClass("symbole_1")
                     i = 0
-                }, 100)
+                }, 300)
+
+                setTimeout(function() {
+                    document.getElementById('winner').innerHTML = "";
+                },2000);
             }
         }
     })
